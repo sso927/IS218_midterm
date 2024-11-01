93ffbf4a184367be069347675402fc9fe49bd6ed
sso927 <sso9@njit.edu>
2024-10-31 20:59:33 -0400

    starting the history features

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod 
 import logging 
 import logging.config
+from history import HistoryCommand
 #contains defintion of the command classes to specific operations 
+
+history = HistoryCommand()
 
 class Command(ABC):
     @abstractmethod 
@@ -14,7 +17,12 @@ class AddCommand():
         self.num2 = num2
 
     def execute(self):
-        return self.num1 + self.num2 
+        answer = self.num1 + self.num2
+        history.log_history(self.num1, self.num2, 'add', answer)
+        return answer
+    
+    
+
 #shows the command defintion for an addition command 
     
 class SubtractCommand():
@@ -23,8 +31,9 @@ class SubtractCommand():
         self.num2 = num2
 
     def execute(self):
-        return self.num1 - self.num2 
-    
+        answer = self.num1 - self.num2
+        history.log_history(self.num1, self.num2, 'subtract', answer)
+        return answer
     
 class MultiplyCommand():
     def __init__(self, num1, num2):
@@ -36,7 +45,9 @@ class MultiplyCommand():
             raise TypeError()
         if not isinstance(self.num2, (int, float)):
             raise TypeError()
-        return self.num1 * self.num2 
+        answer = self.num1 * self.num2
+        history.log_history(self.num1, self.num2, 'multiply', answer)
+        return answer
     
     
 class DivideCommand():
@@ -47,9 +58,11 @@ class DivideCommand():
     def execute(self):
         if self.num2 == 0 :
             raise ZeroDivisionError()
-        return self.num1/self.num2 
+        answer = self.num1 + self.num2
+        history.log_history(self.num1, self.num2, 'divide', answer)
+        return answer
     
-
+    
 class MenuCommand():
     def execute(self):
         menu = "The avaliable plugin commands are as follows: add, subtract, multiply, divide, greet, exit, and menu. To utilize these commands, simply format your input as either <number 1> <number 2> <command> or just the <command>, depending on what you want the program to do."
--- history/__init__.py
+++ history/__init__.py
@@ -1,2 +1,34 @@
 #place code to read history here using pandas
 #it calls from history.csv file 
+#pandas has to write the file and also read the file 
+import pandas as pd
+import logging 
+import logging.config
+
+dataFrame = pd.read_csv('data/history.csv')
+
+class HistoryCommand:
+    def __init__(self, file = 'data/history.csv'):
+        self.file = file 
+        self.history_list = []
+        self.history_df = pd.DataFrame(columns=['number 1', 'number 2', 'operation', 'result'])
+        self.load_history()
+
+
+    def load_history(self):
+        try:
+            self.history = pd.read_csv(self.file)
+        except FileNotFoundError:
+            print('File not found.')
+            logging.error('File has been found and cannot be loaded.')
+
+    def log_history(self, num1, num2, operation, result):
+        self.history_list.append([num1, num2, operation, result])
+        self.history_df = pd.DataFrame(self.history_list, columns=['number 1', 'number 2', 'operation', 'result'])
+
+
+    def save_history(self, file):
+        self.history.to_csv(file, index = False)
+
+    def retrieve_history(self):
+        return self.history 
